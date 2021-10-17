a711ac2f758410536314b15184dce9616651235b
manyids2 <arun.mukundan@gmail.com>
2021-10-17 19:59:45 +0200

    Set languages

--- site/js/app.js
+++ site/js/app.js
@@ -4,7 +4,11 @@
     constructor(config) {
       this.config = config;
       this.story = config.story;
-      this.languages = config.languages;
+      if (window.localStorage.getItem("languages")) {
+        this.languages = window.localStorage.getItem("languages").split(",");
+      } else {
+        window.localStorage.setItem("languages", this.languages.join(","));
+      }
       this.n = this.languages.length;
       this.numLines = 0;
       this.spans = {};
@@ -13,16 +17,34 @@
       this.languages.forEach((language) => {
         this.loading[language] = true;
       });
+      this.title = document.getElementById("story-name");
+      this.title.innerText = this.story;
+      this.switchBtn = document.getElementById("switch-button");
+      this.switchBtn.addEventListener("click", this._cycleOrder);
+      this.addBtn = document.getElementById("add-button");
+      this.addBtn.addEventListener("click", this._handleAddRemove);
+      this.removeBtn = document.getElementById("remove-button");
+      this.removeBtn.addEventListener("click", this._handleAddRemove);
+      this.languageDiv = document.getElementById("language-options");
+      this.languageBack = document.getElementById("language-back");
+      this.languageGo = document.getElementById("language-go");
+      this.languageBack.addEventListener("click", this._handleLanguageBack);
+      this.languageGo.addEventListener("click", this._handleLanguageGo);
+      this.selectedLanguages = {};
+      this.availableLanguages = config.availableLanguages;
+      this.availableLanguages.forEach((language) => {
+        this.selectedLanguages[language] = this.loading[language] ? true : false;
+      });
+      this.languageList = document.getElementById("language-list");
+      this._renderLanguageList();
+      this.storyDiv = document.getElementById("story-options");
+      this.storyList = document.getElementById("story-list");
       this.contentDiv = document.getElementById("ryuu-content");
       this.contentDiv.addEventListener("content-loaded", () => {
         console.log("this.numLines :>> ", this.numLines);
         this.render();
         this.update();
       });
-      this.switchBtn = document.getElementById("switch-button");
-      this.switchBtn.addEventListener("click", this._cycleOrder);
-      this.title = document.getElementById("story-name");
-      this.title.innerText = this.story;
       this._initContent();
     }
     _initContent() {
@@ -41,6 +63,39 @@
         });
       });
     }
+    _handleAddRemoveLanguage = (e) => {
+      const language = e.target.dataset.language;
+      this.selectedLanguages[language] = !this.selectedLanguages[language];
+      this._renderLanguageList();
+    };
+    _renderLanguageList = () => {
+      this.languageList.innerHTML = "";
+      this.availableLanguages.forEach((language) => {
+        const item = document.createElement("li");
+        item.innerText = language;
+        item.setAttribute("data-language", language);
+        if (this.selectedLanguages[language]) {
+          item.classList.add("selected-language");
+        }
+        item.addEventListener("click", this._handleAddRemoveLanguage);
+        this.languageList.appendChild(item);
+      });
+    };
+    _handleLanguageBack = () => {
+      this.languageDiv.style.display = "none";
+    };
+    _handleLanguageGo = () => {
+      const languages = [];
+      for (const language in this.selectedLanguages) {
+        this.selectedLanguages[language] && languages.push(language);
+      }
+      window.localStorage.setItem("languages", languages);
+      location.reload();
+    };
+    _handleAddRemove = () => {
+      console.log("addremove :>> ");
+      this.languageDiv.style.display = "flex";
+    };
     _cycleOrder = () => {
       this.languages = [...this.languages.slice(1), this.languages[0]];
       this.update();
@@ -77,20 +132,23 @@
             this.spans[index][language].classList.add("selected");
             this.spans[index][language].classList.remove("notselected");
             this.spans[index][language].innerText = this.content[language][index];
+            this.spans[index][language].style.width = "calc(85vw - 1rem)";
           } else {
             this.spans[index][language].classList.add("notselected");
             this.spans[index][language].classList.remove("selected");
             this.spans[index][language].innerText = "&nbsp;";
+            this.spans[index][language].style.width = `${1 / (this.languages.length - 1)}rem`;
           }
         });
       }
     }
   };
   window.addEventListener("DOMContentLoaded", () => {
-    window.app = new App({
-      apiUrl: "http://10.0.0.5:7851",
-      languages: ["english", "french"],
-      story: "bakers-dozen-saki"
-    });
+    const config = {
+      languages: ["english", "dutch"],
+      availableLanguages: ["english", "dutch", "french", "german", "spanish", "hindi", "tamil", "bengali"],
+      story: "burial-of-the-minnisink"
+    };
+    window.app = new App(config);
   });
 })();
--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@ class App {
   constructor(config) {
     this.config = config;
     this.story = config.story;
-    this.languages = config.languages;
+
+    if (window.localStorage.getItem("languages")) {
+      this.languages = window.localStorage.getItem("languages").split(",");
+    } else {
+      window.localStorage.setItem("languages", this.languages.join(","));
+    }
+
     this.n = this.languages.length;
     this.numLines = 0;
     this.spans = {};
@@ -12,6 +18,39 @@ class App {
       this.loading[language] = true;
     });
 
+    // Ref to navbar
+    this.title = document.getElementById("story-name");
+    this.title.innerText = this.story;
+
+    // Ref to buttons
+    this.switchBtn = document.getElementById("switch-button");
+    this.switchBtn.addEventListener("click", this._cycleOrder);
+
+    this.addBtn = document.getElementById("add-button");
+    this.addBtn.addEventListener("click", this._handleAddRemove);
+
+    this.removeBtn = document.getElementById("remove-button");
+    this.removeBtn.addEventListener("click", this._handleAddRemove);
+
+    // Modals
+    this.languageDiv = document.getElementById("language-options");
+    this.languageBack = document.getElementById("language-back");
+    this.languageGo = document.getElementById("language-go");
+    this.languageBack.addEventListener("click", this._handleLanguageBack);
+    this.languageGo.addEventListener("click", this._handleLanguageGo);
+
+    // Lists
+    this.selectedLanguages = {};
+    this.availableLanguages = config.availableLanguages;
+    this.availableLanguages.forEach((language) => {
+      this.selectedLanguages[language] = this.loading[language] ? true : false;
+    });
+    this.languageList = document.getElementById("language-list");
+    this._renderLanguageList();
+
+    this.storyDiv = document.getElementById("story-options");
+    this.storyList = document.getElementById("story-list");
+
     // Get ref to container
     this.contentDiv = document.getElementById("ryuu-content");
     this.contentDiv.addEventListener("content-loaded", () => {
@@ -19,14 +58,6 @@ class App {
       this.render();
       this.update();
     });
-
-    // Ref to buttons
-    this.switchBtn = document.getElementById("switch-button");
-    this.switchBtn.addEventListener("click", this._cycleOrder);
-
-    // Ref to navbar
-    this.title = document.getElementById("story-name");
-    this.title.innerText = this.story;
 
     this._initContent();
   }
@@ -49,6 +80,44 @@ class App {
         });
     });
   }
+
+  _handleAddRemoveLanguage = (e) => {
+    const language = e.target.dataset.language;
+    this.selectedLanguages[language] = !this.selectedLanguages[language];
+    this._renderLanguageList();
+  };
+
+  _renderLanguageList = () => {
+    this.languageList.innerHTML = "";
+    this.availableLanguages.forEach((language) => {
+      const item = document.createElement("li");
+      item.innerText = language;
+      item.setAttribute("data-language", language);
+      if (this.selectedLanguages[language]) {
+        item.classList.add("selected-language");
+      }
+      item.addEventListener("click", this._handleAddRemoveLanguage);
+      this.languageList.appendChild(item);
+    });
+  };
+
+  _handleLanguageBack = () => {
+    this.languageDiv.style.display = "none";
+  };
+
+  _handleLanguageGo = () => {
+    const languages = [];
+    for (const language in this.selectedLanguages) {
+      this.selectedLanguages[language] && languages.push(language);
+    }
+    window.localStorage.setItem("languages", languages);
+    location.reload();
+  };
+
+  _handleAddRemove = () => {
+    console.log("addremove :>> ");
+    this.languageDiv.style.display = "flex";
+  };
 
   _cycleOrder = () => {
     this.languages = [...this.languages.slice(1), this.languages[0]];
@@ -91,10 +160,12 @@ class App {
           this.spans[index][language].classList.add("selected");
           this.spans[index][language].classList.remove("notselected");
           this.spans[index][language].innerText = this.content[language][index];
+          this.spans[index][language].style.width = "calc(85vw - 1rem)";
         } else {
           this.spans[index][language].classList.add("notselected");
           this.spans[index][language].classList.remove("selected");
           this.spans[index][language].innerText = "&nbsp;";
+          this.spans[index][language].style.width = `${1 / (this.languages.length - 1)}rem`;
         }
       });
     }
@@ -102,9 +173,11 @@ class App {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  window.app = new App({
-    apiUrl: "http://10.0.0.5:7851",
-    languages: ["english", "french"],
-    story: "bakers-dozen-saki",
-  });
+  const config = {
+    languages: ["english", "dutch"],
+    availableLanguages: ["english", "dutch", "french", "german", "spanish", "hindi", "tamil", "bengali"],
+    story: "burial-of-the-minnisink",
+  };
+
+  window.app = new App(config);
 });
